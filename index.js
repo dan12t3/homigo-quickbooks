0fa4ab4ff6c131e27e546b51fb9de6a947c8ada6
dan12t3 <dan12t3@gmail.com>
2017-07-13 20:47:04 -0400

    first deployment

--- index.js
+++ index.js
@@ -1,36 +1,22 @@
 const express = require('express');
 const http = require('http');
+const console = require('console');
+const crypto = require('crypto');
+const querystring = require('querystring');
+//const bodyParser = require('body-parser');
 
 let app = express();
 
-
-
-
-
-//functions
-
-function getRequestToken(){
-  var options = {
-
-  }
-
-  http.get(options, (response) => {
-
-  });
-
-}
-
-// make a call to get request token - http.get
-    //parameters
-    //oauth_callback
-    //oauth_nonce
-    //timestamp
-
-
-
-// handling
+const grantURL = 'kuikview-backend.herokuapp.com/oauth-redirect';
+const consumerKey = 'qyprdpCFmIezychyaIgYRuY6osoApE';
 
 // listening on a PORT
+/*app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+app.use(bodyParser.json());
+*/
 app.listen( process.env.PORT || 5000, (err) => {
   if(err){
     console.log(err);
@@ -38,3 +24,51 @@ app.listen( process.env.PORT || 5000, (err) => {
     console.log("listening on: ",process.env.PORT || '5000');
   }
 });
+
+//functions
+app.get('/oauth-redirect',(request, response) => {
+  console.log(request.query);
+  response.end(request.query);
+});
+
+app.get('/',(request,response) => {
+  getRequestToken();
+  response.end("ok");
+});
+
+
+// test case
+/*app.get('/oauth/v1/get_request_token',(request,response) =>{
+  console.log(request.query);
+  response.send('ok');
+});*/
+
+function getRequestToken(){
+  //sending to
+  //what im sending
+  const data = querystring.stringify({
+    'oauth_callback': grantURL,
+    'oauth_consumer_key': consumerKey,
+    'oauth_nonce': crypto.randomBytes(16).toString('hex'),
+    'oauth_signature': 'eqjbD6s%2FZiZy8FiNU9uoaspF6Q4%3D&',
+    'oauth_signature_method': 'HMAC-SHA1&',
+    'oauth_timestamp': +new Date(), //doesnt work
+    'oauth_version': '1.0'
+  });
+
+  const options = {
+    host: 'oauth.intuit.com',
+    path: '/oauth/v1/get_request_token?' + data,
+    method: 'GET',
+    port: 80,
+  }
+
+  let req = http.request(options, (response) => {
+    console.log("Response Code:",response.statusCode);
+
+  }).on('error',(err)=>{
+    console.log("Error:",err);
+  });
+
+  req.end();
+}
